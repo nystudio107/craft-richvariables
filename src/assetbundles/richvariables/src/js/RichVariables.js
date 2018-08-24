015587da30058c39b4089b1d06df211118f3688a
Andrew Welch <andrew@nystudio107.com>
2018-08-24 00:37:02 -0400

    Clean up JS
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/richvariables/src/js/RichVariables.js
+++ src/assetbundles/richvariables/src/js/RichVariables.js
@@ -1,14 +1,7 @@
-/**
- * Rich Variables plugin for Craft CMS
- *
- * RichVariables JS
- *
- * @author    nystudio107
- * @copyright Copyright (c) 2017 nystudio107
- * @link      https://nystudio107.com
- * @package   RichVariables
- * @since     1.0.0
- */
-
 // Import our CSS
 import styles from '../css/app.pcss';
+
+const main = async () => {
+};
+
+main().then({});
