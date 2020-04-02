f33858d99dbac7c72307f1cd77f9118acf9977c7
Andrew Welch <andrew@keluli.local>
2020-04-02 11:45:55 -0400

    Updated assets build

--- src/assetbundles/richvariables/dist/js/styles.js
+++ src/assetbundles/richvariables/dist/js/styles.js
@@ -2,9 +2,9 @@
  * @project        Rich Variables
  * @name           styles.js
  * @author         undefined
- * @build          Mon, Nov 4, 2019 3:59 PM ET
- * @release        efa340b619b22751fcd3752cb0103b40b21b11bd [develop]
- * @copyright      Copyright (c) 2019 nystudio107
+ * @build          Thu, Apr 2, 2020 3:44 PM ET
+ * @release        618bde4f3713223bc1d4c7c26ebaa22464f57b2c [develop]
+ * @copyright      Copyright (c) 2020 nystudio107
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],{8:function(n,w,o){}}]);
